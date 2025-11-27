fee11e5398bec52ac3da8eefe1e4fccb3353ae95
yasirali23 <yasir@srptechs.com>
2025-11-27 12:44:24 +0500

    Update REACT_APP_BASE_URL to new API endpoint

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
       values: {
         __DEV__: JSON.stringify(true),
         "process.env.NODE_ENV": JSON.stringify("development"),
-        "process.env.REACT_APP_BASE_URL": JSON.stringify("https://ksvvmxbk-3200.inc1.devtunnels.ms/api"),
+        "process.env.REACT_APP_BASE_URL": JSON.stringify("https://51.20.119.116:8443/api"),
          //"process.env.REACT_APP_BASE_URL": JSON.stringify("http://13.51.230.148:3000/api"),
       },
     }),
